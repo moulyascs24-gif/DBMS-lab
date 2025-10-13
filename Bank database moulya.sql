ee03b2e8401cdec2506a41617248174f078ec486
moulyascs24-gif <moulyas.cs24@bmsce.ac.in>
2025-10-13 12:11:22 +0530

    Add files via upload

---
+++ Bank database moulya.sql
@@ -0,0 +1,53 @@
+USE newdatabase;
+create table branch(Branch_name varchar(30),Branch_city varchar(25),assets int,PRIMARY KEY (Branch_name));
+desc branch; 
+insert into branch values('SBI_Chamrajpet','Bangalore','50000');
+insert into branch values('SBI_ResidencyRoad','Bangalore','10000');
+insert into branch values('SBI_ShivajiRoad','Bombay','20000');
+insert into branch values('SBI_ParlimentRoad','Delhi','10000');
+insert into branch values('SBI_Jantarmantar','Delhi','20000');
+select* from branch;
+
+create table BankAccount(Accno int,Branch_name varchar(30),Balance int,PRIMARY KEY(Accno),foreign key (Branch_name) references branch(Branch_name));
+desc BankAccount;
+insert into BankAccount values('1','SBI_Chamrajpet','2000');
+insert into BankAccount values('2','SBI_ResidencyRoad','5000');
+insert into BankAccount values('3','SBI_ShivajiRoad','6000');
+insert into BankAccount values('4','SBI_ParlimentRoad','9000');
+insert into BankAccount values('5','SBI_Jantarmantar','8000');
+insert into BankAccount values('6','SBI_ShivajiRoad','4000');
+insert into BankAccount values('8','SBI_ResidencyRoad','4000');
+insert into BankAccount values('9','SBI_ParlimentRoad','3000');
+insert into BankAccount values('10','SBI_ResidencyRoad','5000');
+insert into BankAccount values('11','SBI_Jantarmantar','2000');
+select* from BankAccount;
+
+create table BankCustomer(Customername varchar(20),Customer_street varchar(30),CustomerCity varchar (35),PRIMARY KEY(Customername));
+desc BankCustomer;
+insert into BankCustomer values('Avinash','Bull_Temple_Road','Bangalore');
+insert into BankCustomer values('Dinesh','Bannergatta_Road','Bangalore');
+insert into BankCustomer values('Mohan','NationalCollege_Road','Bangalore');
+insert into BankCustomer values('Nikil','Akbar_Road','Delhi');
+insert into BankCustomer values('Ravi','Prithviraj_Road','Delhi');
+select* from BankCustomer;
+
+create table Depositer(Customername varchar(20),Accno int,PRIMARY KEY(Customername,Accno),foreign key (Accno) references BankAccount(Accno),foreign key (Customername) references BankCustomer(Customername));
+desc Depositer;
+insert into Depositer values('Avinash','1');
+insert into Depositer values('Dinesh','2');
+insert into Depositer values('Nikil','4');
+insert into Depositer values('Ravi','5');
+insert into Depositer values('Avinash','8');
+insert into Depositer values('Nikil','9');
+insert into Depositer values('Dinesh','10');
+insert into Depositer values('Nikil','11');
+select* from Depositer;
+
+create table Loan(Loan_number int,Branch_name varchar(30),Amount int,PRIMARY KEY(Loan_number),foreign key (Branch_name) references branch(Branch_name));
+desc Loan;
+insert into Loan values('1','SBI_Chamrajpet','1000');
+insert into Loan values('2','SBI_ResidencyRoad','2000');
+insert into Loan values('3','SBI_ShivajiRoad','3000');
+insert into Loan values('4','SBI_ParlimentRoad','4000');
+insert into Loan values('5','SBI_Jantarmantar','5000');
+select* from Loan;
